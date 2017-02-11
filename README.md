f3436c109182be862fcea29324f236bde4de9ad7
Joonas Rihma <jrihma@klass2-22-l.intra.itcollege.ee>
2017-02-11 16:21:55 +0200

    esimene

---
+++ README.md
@@ -0,0 +1,2 @@
+# Vorgurakendused
+# Vorgurakendused
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<title> Teretulemast kodukale</title>
+	<meta charset="UTF-8">
+
+<link type="text/css"  rel="stylesheet" href="stiilifail.css" />
+  </head>
+  <body>
+    	
+	
+	<img src="//enos.itcollege.ee/~jrihma/pildid/rebane.jpg"alt="rebane"/>
+	<img src="/~jrihma/pildid/rebane.jpg" alt="rebane"/>
+	<img src="pildid/rebane.jpg" alt="rebane"/>
+
+			
+	<a href="https://google.ee">Google</a>
+
+	<?php	echo "See tekst tuleb phpst"; echo phpversion(); // katse?>
+   </body>
+</html>
